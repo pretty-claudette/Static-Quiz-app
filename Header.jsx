8d88c992663dbd297b2e025cb8cff87ccb81c8da
pretty-klodhet <claudettelladones26@gmail.com>
2025-02-20 14:08:35 +0800

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div className='text-center flex-row justify-center mt-0'>
+      <h1 className='text-base font-medium mb-3'>Static Quiz app</h1>
+      <p className='text-sm font-medium'>project Name: quiz-app</p>
+    </div>
+  )
+}
+
+export default Header
